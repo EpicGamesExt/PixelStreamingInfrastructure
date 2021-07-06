ca0cd0a4f0e93196bd4c7ec4244ed416557e4bda
Juan Navarro <juan.navarro@gmx.es>
2021-07-06 17:42:11 +0200

    delete lib/ and use a prepare script instead
    If a package being installed through git contains a prepare script, its
    dependencies and devDependencies will be installed, and the prepare
    script will be run, before the package is packaged and installed.
    
    https://docs.npmjs.com/cli/v7/using-npm/scripts#life-cycle-scripts

--- lib/SdpUtils.d.ts
+++
@@ -1,4 +0,0 @@
-import { RtpParameters, RtpCapabilities, MediaKind } from "mediasoup/lib/types";
-export declare function sdpToRecvRtpCapabilities(sdpObject: object, localCaps: RtpCapabilities): RtpCapabilities;
-export declare function sdpToSendRtpParameters(sdpObject: any, localCaps: RtpCapabilities, kind: MediaKind): RtpParameters;
-//# sourceMappingURL=SdpUtils.d.ts.map
