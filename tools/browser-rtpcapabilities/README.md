83ddeef7a0663328b6fa7ed2d7d1db685b802e98
Juan Navarro <juan.navarro@gmx.es>
2023-09-22 13:26:21 +0200

    browser-rtpcapabilities: Show more details; add RtpParameters

--- tools/browser-rtpcapabilities/README.md
+++ tools/browser-rtpcapabilities/README.md
@@ -5,10 +5,15 @@ This tool generates default SDP Offer/Answer messages from the web browser you u
 Run it like this:
 
 ```sh
+cd mediasoup-sdp-bridge/
+npm install
+npm run build
+
+cd tools/browser-rtpcapabilities/
 npm install
 npm run start
 ```
 
 Now, open a web browser at http://127.0.0.1:8080, and click on *Run*.
 
-The resulting text is source code for a JavaScript object, which you can copy-paste into the file [mediasoup-sdp-bridge/src/BrowserRtpCapabilities.ts](../../src/BrowserRtpCapabilities.ts).
+The topmost output is source code for a JavaScript object, which you can copy-paste into the file [mediasoup-sdp-bridge/src/BrowserRtpCapabilities.ts](../../src/BrowserRtpCapabilities.ts).
--- tools/browser-rtpcapabilities/src/client/index.js
+++ tools/browser-rtpcapabilities/src/client/index.js
@@ -1,15 +1,23 @@
 "use strict";
 
+const JSON5 = require("json5");
+
 const MsSdpUtils = require("mediasoup-client/lib/handlers/sdp/commonUtils");
 const SdpTransform = require("sdp-transform");
-const JSON5 = require("json5");
+const SdpUtils = require("mediasoup-sdp-bridge/lib/SdpUtils");
 
 const ui = {
   run: document.getElementById("run"),
-  output: document.getElementById("output"),
+
+  recvSdp: document.getElementById("recvSdp"),
+  recvCaps: document.getElementById("recvCaps"),
+
+  sendSdp: document.getElementById("sendSdp"),
+  sendAudioParams: document.getElementById("sendAudioParams"),
+  sendVideoParams: document.getElementById("sendVideoParams"),
 };
 
-ui.run.addEventListener("click", startSdp);
+ui.run.addEventListener("click", run);
 
 window.addEventListener("load", () => {
   console.log("[window.on('load')] Page loaded");
@@ -26,7 +34,7 @@ window.addEventListener("load", () => {
   }
 });
 
-async function startSdp() {
+async function run() {
   const pc = new RTCPeerConnection();
 
   pc.addTransceiver("audio", {
@@ -41,19 +49,61 @@ async function startSdp() {
   await pc.setRemoteDescription(sdpOffer);
   const sdpAnswer = await pc.createAnswer();
 
-  printRtpCapabilities(sdpAnswer.sdp);
+  processAnswer(sdpAnswer.sdp);
+  processOffer(sdpOffer.sdp);
 }
 
-function printRtpCapabilities(sdp) {
-  const sdpObject = SdpTransform.parse(sdp);
+// Generate RtpCapabilities from SDP Answer.
+function processAnswer(sdpAnswer) {
+  const sdpObject = SdpTransform.parse(sdpAnswer);
+  const caps = MsSdpUtils.extractRtpCapabilities({ sdpObject });
+  const capsStr = JSON5.stringify(caps, { space: 2, quote: `"` });
+  const capsText = `export const ${adapter.browserDetails.browser}: RtpCapabilities = ${capsStr}`;
+
+  console.log(`recvSdp:\n${sdpAnswer}`);
+  console.log(capsText);
+
+  ui.recvSdp.innerHTML = sdpAnswer;
+  ui.recvCaps.innerHTML = capsText;
+
+  // Optional. Run if you're curious about the Consumer caps.
+  //const consumerCaps = SdpUtils.sdpToConsumerRtpCapabilities(sdpObject, caps);
+  //const consumerCapsStr = JSON5.stringify(consumerCaps, { space: 2, quote: `"` });
+  //console.log(`consumerCaps:\n${consumerCapsStr}`);
+}
+
+// Generate RtpParameters from SDP Offer.
+function processOffer(sdpOffer) {
+  const sdpObject = SdpTransform.parse(sdpOffer);
+
+  // sdp-transform bug #94: Type inconsistency in payloads
+  // https://github.com/clux/sdp-transform/issues/94
+  // Force "payloads" to be a string field.
+  for (const media of sdpObject.media) {
+    media.payloads = "" + media.payloads;
+  }
 
   const caps = MsSdpUtils.extractRtpCapabilities({ sdpObject });
-  //const capsStr = JSON5.stringify(caps, null, 2); // Uses single quotes, `'`.
-  const capsStr = JSON5.stringify(caps, { space: 2, quote: `"` });
 
-  const text = `export const ${
-    window.adapter?.browserDetails.browser ?? "browser"
-  }: RtpCapabilities = ${capsStr};`;
-  console.log(text);
-  ui.output.innerHTML = text;
+  const audioParams = SdpUtils.sdpToProducerRtpParameters(
+    sdpObject,
+    caps,
+    "audio"
+  );
+  const audioParamsStr = JSON5.stringify(audioParams, { space: 2, quote: `"` });
+
+  const videoParams = SdpUtils.sdpToProducerRtpParameters(
+    sdpObject,
+    caps,
+    "video"
+  );
+  const videoParamsStr = JSON5.stringify(videoParams, { space: 2, quote: `"` });
+
+  console.log(`sendSdp:\n${sdpOffer}`);
+  console.log(`audioParams:\n${audioParamsStr}`);
+  console.log(`videoParams:\n${videoParamsStr}`);
+
+  ui.sendSdp.innerHTML = sdpOffer;
+  ui.sendAudioParams.innerHTML = audioParamsStr;
+  ui.sendVideoParams.innerHTML = videoParamsStr;
 }
