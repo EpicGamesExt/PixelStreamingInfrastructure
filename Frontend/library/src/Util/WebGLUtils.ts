9897448900f742a7d3c1df729bc24fc1886cfc2e
Luke Bermingham <1215582+lukehb@users.noreply.github.com>
2024-04-07 17:46:03 +1000

    Refactored WebXR rendering to use WebXR layers API available of Meta Quest browser. This allows the use of timewarp and improves latency in headset.

--- Frontend/library/src/Util/WebGLUtils.ts
+++ Frontend/library/src/Util/WebGLUtils.ts
@@ -7,26 +7,16 @@ export class WebGLUtils {
 		attribute vec2 a_texCoord;
 
 		// input
-		uniform vec2 u_resolution;
-		uniform vec4 u_offset;
+		uniform vec4 u_texOffset;
 
-		//
+		// varyings
 		varying vec2 v_texCoord;
 
 		void main() {
-		   // convert the rectangle from pixels to 0.0 to 1.0
-		   vec2 zeroToOne = a_position / u_resolution;
-
-		   // convert from 0->1 to 0->2
-		   vec2 zeroToTwo = zeroToOne * 2.0;
-
-		   // convert from 0->2 to -1->+1 (clipspace)
-		   vec2 clipSpace = zeroToTwo - 1.0;
-
-		   gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
+		   gl_Position = vec4(a_position.x, a_position.y, 0, 1);
 		   // pass the texCoord to the fragment shader
 		   // The GPU will interpolate this value between points.
-		   v_texCoord = (a_texCoord * u_offset.xy) + u_offset.zw;
+		   v_texCoord = (a_texCoord * u_texOffset.xy) + u_texOffset.zw;
 		}
 		`;
     }
--- Frontend/library/src/WebXR/WebXRController.ts
+++ Frontend/library/src/WebXR/WebXRController.ts
@@ -3,36 +3,41 @@
 import { Logger } from '../Logger/Logger';
 import { WebRtcPlayerController } from '../WebRtcPlayer/WebRtcPlayerController';
 import { WebGLUtils } from '../Util/WebGLUtils';
-import { Controller } from '../Inputs/GamepadTypes';
 import { XRGamepadController } from '../Inputs/XRGamepadController';
 import { XrFrameEvent } from '../Util/EventEmitter'
 import { Flags } from '../pixelstreamingfrontend';
+import WebXRLayersPolyfill from '@epicgames-ps/webxr-layers-polyfill';
 
 export class WebXRController {
     private xrSession: XRSession;
     private xrRefSpace: XRReferenceSpace;
     private gl: WebGL2RenderingContext;
+    private xrFramebuffer: WebGLFramebuffer;
+    private xrGLFactory: XRWebGLBinding;
+    private xrProjectionLayer: XRProjectionLayer;
 
     private positionLocation: number;
     private texcoordLocation: number;
-    private resolutionLocation: WebGLUniformLocation;
-    private offsetLocation: WebGLUniformLocation;
+    private textureOffsetUniform: WebGLUniformLocation;
 
     private positionBuffer: WebGLBuffer;
     private texcoordBuffer: WebGLBuffer;
 
     private webRtcController: WebRtcPlayerController;
     private xrGamepadController: XRGamepadController;
-    private xrControllers: Array<Controller>;
+
+    // Ignore unused, simply initializing this polyfill patches browser API for browser's
+    // that do not support the WebXR layers API.
+    private xrLayersPolyfill: WebXRLayersPolyfill;
 
     onSessionStarted: EventTarget;
     onSessionEnded: EventTarget;
     onFrame: EventTarget;
 
     constructor(webRtcPlayerController: WebRtcPlayerController) {
+        this.xrLayersPolyfill = new WebXRLayersPolyfill();
         this.xrSession = null;
         this.webRtcController = webRtcPlayerController;
-        this.xrControllers = [];
         this.xrGamepadController = new XRGamepadController(
             this.webRtcController.streamMessageController
         );
@@ -44,7 +49,8 @@ export class WebXRController {
     public xrClicked() {
         if (!this.xrSession) {
             navigator.xr
-                .requestSession('immersive-vr')
+                /* Enable WebXR layers as user agent's which support this can optimize rendering path. */
+                .requestSession('immersive-vr', { optionalFeatures: ['layers'] })
                 .then((session: XRSession) => {
                     this.onXrSessionStarted(session);
                 });
@@ -59,23 +65,16 @@ export class WebXRController {
         this.onSessionEnded.dispatchEvent(new Event('xrSessionEnded'));
     }
 
-    onXrSessionStarted(session: XRSession) {
-        Logger.Log(Logger.GetStackTrace(), 'XR Session started');
-
-        this.xrSession = session;
-        this.xrSession.addEventListener('end', () => {
-            this.onXrSessionEnded();
-        });
-
+    initGL() {
+        if (this.gl) { return; }
         const canvas = document.createElement('canvas');
         this.gl = canvas.getContext('webgl2', {
             xrCompatible: true
         });
+        this.gl.clearColor(0.0, 0.0, 0.0, 1);
+    }
 
-        this.xrSession.updateRenderState({
-            baseLayer: new XRWebGLLayer(this.xrSession, this.gl)
-        });
-
+    initShaders() {
         // setup vertex shader
         const vertexShader = this.gl.createShader(this.gl.VERTEX_SHADER);
         this.gl.shaderSource(vertexShader, WebGLUtils.vertexShader());
@@ -102,15 +101,18 @@ export class WebXRController {
             shaderProgram,
             'a_texCoord'
         );
-        // Create a buffer to put three 2d clip space points in
-        this.positionBuffer = this.gl.createBuffer();
-        // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.positionBuffer);
+        // lookup uniforms
+        this.textureOffsetUniform = this.gl.getUniformLocation(
+            shaderProgram,
+            'u_texOffset'
+        );
+    }
 
-        // Turn on the position attribute
-        this.gl.enableVertexAttribArray(this.positionLocation);
-        // Create a texture.
-        const texture = this.gl.createTexture();
+    initTexture(){
+        // Create our texture that we use in our shader
+        // and bind it once because we never use any other texture.
+
+        const texture: WebGLTexture = this.gl.createTexture();
         this.gl.bindTexture(this.gl.TEXTURE_2D, texture);
         // Set the parameters so we can render any size image.
         this.gl.texParameteri(
@@ -133,24 +135,109 @@ export class WebXRController {
             this.gl.TEXTURE_MAG_FILTER,
             this.gl.NEAREST
         );
+    }
 
-        this.texcoordBuffer = this.gl.createBuffer();
-        // lookup uniforms
-        this.resolutionLocation = this.gl.getUniformLocation(
-            shaderProgram,
-            'u_resolution'
-        );
-        this.offsetLocation = this.gl.getUniformLocation(
-            shaderProgram,
-            'u_offset'
-        );
+    initBuffers(){
+        // Bind the frame buffer we want to draw into.
+        // Only need to bind it once as we reuse it for each eye.
+        this.xrFramebuffer = this.gl.createFramebuffer();
+        this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, this.xrFramebuffer);
+
+        // Create out position buffer and its vertex shader attribute
+        {
+            // Create a buffer to put the the vertices of the plane we will draw the video stream onto
+            this.positionBuffer = this.gl.createBuffer();
+            // Bind the position buffer
+            this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.positionBuffer);
+            // Enable `positionLocation` to be used as vertex shader attribute
+            this.gl.enableVertexAttribArray(this.positionLocation);
+
+            // Note: positions are passed in clip-space coordinates [-1..1] so no need to convert in-shader
+            // prettier-ignore
+            this.gl.bufferData(
+                this.gl.ARRAY_BUFFER,
+                new Float32Array([
+                    -1.0,  1.0,
+                     1.0,  1.0,
+                    -1.0, -1.0,
+                    -1.0, -1.0,
+                     1.0,  1.0,
+                     1.0, -1.0
+                ]),
+                this.gl.STATIC_DRAW
+            );
+
+            // Tell position attribute of the vertex shader how to get data out of the bound buffer (the positionBuffer)
+            this.gl.vertexAttribPointer(
+                this.positionLocation,
+                2 /*size*/,
+                this.gl.FLOAT /*type*/,
+                false /*normalize*/,
+                0 /*stride*/,
+                0 /*offset*/
+            );
+        }
+
+        // Create our texture coordinate buffers for accessing our texture
+        {
+            this.texcoordBuffer = this.gl.createBuffer();
+            // Bind the texture coordinate buffer
+            this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.texcoordBuffer);
+            // Enable `texcoordLocation` to be used as a vertext shader attribute
+            this.gl.enableVertexAttribArray(this.texcoordLocation);
+
+            // The texture coordinates to apply for rectangle we are drawing
+            this.gl.bufferData(
+                this.gl.ARRAY_BUFFER,
+                new Float32Array([
+                    0.0, 0.0,
+                    1.0, 0.0,
+                    0.0, 1.0,
+                    0.0, 1.0,
+                    1.0, 0.0,
+                    1.0, 1.0
+                ]),
+                this.gl.STATIC_DRAW
+            );
+
+            // Tell texture coordinate attribute of the vertex shader how to get data out of the bound buffer (the texcoordBuffer)
+            this.gl.vertexAttribPointer(
+                this.texcoordLocation,
+                2 /*size*/,
+                this.gl.FLOAT /*type*/,
+                false /*normalize*/,
+                0 /*stride*/,
+                0 /*offset*/
+            );
+        }
+    }
+
+    onXrSessionStarted(session: XRSession) {
+        Logger.Log(Logger.GetStackTrace(), 'XR Session started');
+
+        this.xrSession = session;
+        this.xrSession.addEventListener('end', () => {
+            this.onXrSessionEnded();
+        });
+
+        // Initialization
+        this.initGL();
+        this.initShaders();
+        this.initBuffers();
+        this.initTexture();
+
+        this.xrGLFactory = new XRWebGLBinding(this.xrSession, this.gl);
 
         session.requestReferenceSpace('local').then((refSpace) => {
             this.xrRefSpace = refSpace;
-            this.xrSession.requestAnimationFrame(
-                (time: DOMHighResTimeStamp, frame: XRFrame) =>
-                    this.onXrFrame(time, frame)
-            );
+
+            // Set up the projection layer, this fills the entire XR viewport.
+            this.xrProjectionLayer = this.xrGLFactory.createProjectionLayer({ textureType: "texture" });
+            this.xrSession.updateRenderState({ layers: [this.xrProjectionLayer] });
+
+            // When WebXR "layers" mode is turned on requesting frames this way will still allow timewarp on devices that support it.
+            // See: https://developer.oculus.com/blog/achieve-better-rendering-and-performance-with-webxr-layers-in-oculus-browser/
+            this.xrSession.requestAnimationFrame(this.onXrFrame.bind(this));
         });
 
         this.onSessionStarted.dispatchEvent(new Event('xrSessionStarted'));
@@ -160,8 +247,7 @@ export class WebXRController {
         // Extract HMD transform matrix and convert from row-major to column-major before sending
         const mat = pose.transform.matrix;
 
-        // Send HMD transform matrix to UE
-        // prettier-ignore
+        // No longer send the XRHMDTransform as the below `XREyeViews` contains all the relevant information.
         // this.webRtcController.streamMessageController.toStreamerHandlers.get('XRHMDTransform')([
         //     mat[0], mat[4], mat[8], mat[12],
         //     mat[1], mat[5], mat[9], mat[13],
@@ -222,13 +308,8 @@ export class WebXRController {
         if (pose) {
             this.sendXRDataToUE(pose);
 
-            const glLayer = this.xrSession.renderState.baseLayer;
-            // If we do have a valid pose, bind the WebGL layer's framebuffer,
-            // which is where any content to be displayed on the XRDevice must be
-            // rendered.
-            this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, glLayer.framebuffer);
-
-            // Upload the image into the texture. WebGL knows how to extract the current frame from the video element
+            // Make video texture available to shader
+            // Only need to make the texture available once as same shader is used each eye
             this.gl.texImage2D(
                 this.gl.TEXTURE_2D,
                 0,
@@ -237,7 +318,12 @@ export class WebXRController {
                 this.gl.UNSIGNED_BYTE,
                 this.webRtcController.videoPlayer.getVideoElement()
             );
-            this.render(this.webRtcController.videoPlayer.getVideoElement());
+
+            // Go through each eye and render to the layer's "subimage" color buffer
+            for(let view of pose.views){
+                // Draw each half the video element's texture to each eye.
+                this.render(view);
+            }
         }
 
         if (this.webRtcController.config.isFlagEnabled(Flags.XRControllerInput)) {
@@ -264,96 +350,34 @@ export class WebXRController {
         }));
     }
 
-    private render(videoElement: HTMLVideoElement) {
+    private render(eyeView: XRView) {
         if (!this.gl) {
             return;
         }
+        if(eyeView.eye === 'none') {
+            // monoscopic rendering not implemented.
+            return;
+        }
 
-        const glLayer = this.xrSession.renderState.baseLayer;
+        // set framebuffer's color buffer to be the eye's color buffer
+        const eyeSubImage: XRWebGLSubImage = this.xrGLFactory.getViewSubImage(this.xrProjectionLayer, eyeView);
+        this.gl.framebufferTexture2D(this.gl.FRAMEBUFFER, this.gl.COLOR_ATTACHMENT0, this.gl.TEXTURE_2D, eyeSubImage.colorTexture, 0);
+
+        // Set the relevant portion of clip space
         this.gl.viewport(
-            0,
-            0,
-            glLayer.framebufferWidth,
-            glLayer.framebufferHeight
-        );
-        this.gl.uniform4f(this.offsetLocation, 1.0, 1.0, 0.0, 0.0);
-
-        // Set rectangle
-        // prettier-ignore
-        this.gl.bufferData(
-            this.gl.ARRAY_BUFFER,
-            new Float32Array([
-                0, 0, 
-                videoElement.videoWidth, 0,
-                0, videoElement.videoHeight, 
-                0, videoElement.videoHeight,
-                videoElement.videoWidth, 0,
-                videoElement.videoWidth, videoElement.videoHeight
-            ]),
-            this.gl.STATIC_DRAW
+            eyeSubImage.viewport.x,
+            eyeSubImage.viewport.y,
+            eyeSubImage.viewport.width,
+            eyeSubImage.viewport.height
         );
 
-        // Provide texture coordinates for the rectangle
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.texcoordBuffer);
-        this.gl.bufferData(
-            this.gl.ARRAY_BUFFER,
-            new Float32Array([
-                0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0
-            ]),
-            this.gl.STATIC_DRAW
-        );
+        const uTexOffset = eyeView.eye === "left" ? 0.0 : 0.5;
 
-        let size; // components per iteration
-        let type; // the data type
-        let normalize; // normalize the data
-        let stride; // 0 = move forward size * sizeof(type) each iteration to get the next position
-        let offset; // start position of the buffer
+        // Set uniform to offset the texture based on which eye we are rendering
+        this.gl.uniform4f(this.textureOffsetUniform, 0.5, 1.0, uTexOffset, 0.0);
 
-        // Bind the position buffer.
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.positionBuffer);
-        // Tell the position attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-        size = 2; // 2 components per iteration
-        type = this.gl.FLOAT; // the data is 32bit floats
-        normalize = false; // don't normalize the data
-        stride = 0; // 0 = move forward size * sizeof(type) each iteration to get the next position
-        offset = 0; // start at the beginning of the buffer
-        this.gl.vertexAttribPointer(
-            this.positionLocation,
-            size,
-            type,
-            normalize,
-            stride,
-            offset
-        );
-        // Turn on the texcoord attribute
-        this.gl.enableVertexAttribArray(this.texcoordLocation);
-        // bind the texcoord buffer.
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.texcoordBuffer);
-        // Tell the texcoord attribute how to get data out of texcoordBuffer (ARRAY_BUFFER)
-        size = 2; // 2 components per iteration
-        type = this.gl.FLOAT; // the data is 32bit floats
-        normalize = false; // don't normalize the data
-        stride = 0; // 0 = move forward size * sizeof(type) each iteration to get the next position
-        offset = 0; // start at the beginning of the buffer
-        this.gl.vertexAttribPointer(
-            this.texcoordLocation,
-            size,
-            type,
-            normalize,
-            stride,
-            offset
-        );
-        // set the resolution
-        this.gl.uniform2f(
-            this.resolutionLocation,
-            videoElement.videoWidth,
-            videoElement.videoHeight
-        );
-        // draw the rectangle.
-        const primitiveType = this.gl.TRIANGLES;
-        const count = 6;
-        offset = 0;
-        this.gl.drawArrays(primitiveType, offset, count);
+        // Draw the rectangle we will show the video stream texture on
+        this.gl.drawArrays(this.gl.TRIANGLES /*primitiveType*/, 0 /*offset*/, 6 /*count*/);
     }
 
     static isSessionSupported(mode: XRSessionMode): Promise<boolean> {
