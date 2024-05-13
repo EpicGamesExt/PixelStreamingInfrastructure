230df1229be6f1bc222139648d091a0686a761b4
Matthew Cotton <matt@tensorworks.com.au>
2024-05-13 15:28:22 +1000

    Fixing linting errors.

--- Signalling/src/SignallingServer.ts
+++ Signalling/src/SignallingServer.ts
@@ -154,6 +154,7 @@ export class SignallingServer {
         // because peer connection options is a general field with all optional fields
         // it doesnt play nice with mergePartial so we just add it verbatim
         const message:Messages.config = MessageHelpers.createMessage(Messages.config, this.protocolConfig);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
         message.peerConnectionOptions = this.protocolConfig.peerConnectionOptions;
         newStreamer.sendMessage(message);
     }
@@ -179,6 +180,7 @@ export class SignallingServer {
         // because peer connection options is a general field with all optional fields
         // it doesnt play nice with mergePartial so we just add it verbatim
         const message:Messages.config = MessageHelpers.createMessage(Messages.config, this.protocolConfig);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
         message.peerConnectionOptions = this.protocolConfig.peerConnectionOptions;
         newPlayer.sendMessage(message);
     }
@@ -198,6 +200,7 @@ export class SignallingServer {
         // because peer connection options is a general field with all optional fields
         // it doesnt play nice with mergePartial so we just add it verbatim
         const message:Messages.config = MessageHelpers.createMessage(Messages.config, this.protocolConfig);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
         message.peerConnectionOptions = this.protocolConfig.peerConnectionOptions;
         newSFU.sendMessage(message);
     }
