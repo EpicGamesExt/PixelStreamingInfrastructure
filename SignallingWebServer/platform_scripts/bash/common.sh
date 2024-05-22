3a7aa20ca96486a408f56c73e2486658a4bfc1ff
Matthew Cotton <matt@tensorworks.com.au>
2024-05-22 16:15:38 +1000

    Adding linux fast startup

--- SignallingWebServer/platform_scripts/bash/common.sh
+++ SignallingWebServer/platform_scripts/bash/common.sh
@@ -24,6 +24,7 @@ function print_usage() {
                             Default value as above
         --build             Force a rebuild of the typescript frontend even if it already exists
         --frontend-dir      Sets the output path for the fontend build
+        --dev               Dev mode. (forces build of wilbur and its dependencies)
 
     Other options: stored and passed to the server.  All parameters printed once the script values are set.
     Command line options might be omitted to run with defaults and it is a good practice to omit specific ones when just starting the TURN or the STUN server alone, not the whole set of scripts.
@@ -38,6 +39,7 @@ function parse_args() {
     FORCE_BUILD=0
     DEFAULT_STUN=0
     DEFAULT_TURN=0
+    BUILD_WILBUR=0
     while(($#)) ; do
         case "$1" in
         --debug ) IS_DEBUG=1; shift;;
@@ -53,6 +55,7 @@ function parse_args() {
         --start-turn ) START_TURN=1; shift;;
         --publicip ) PUBLIC_IP="$2"; shift 2;;
         --frontend-dir ) FRONTEND_DIR="$(realpath "$2")"; shift 2;;
+        --dev ) BUILD_WILBUR=1; shift;;
         --help ) print_usage;;
         * ) SERVER_ARGS+=" $1"; shift;;
         esac
@@ -130,7 +133,9 @@ function check_and_install() { #dep_name #get_version_string #version_min #insta
 
 		if [ $? -ge 1 ]; then
 			echo "Installation of $1 failed try running `export VERBOSE=1` then run this script again for more details"
+            return -1
 		fi
+        return 1
 	fi
 }
 
@@ -175,9 +180,11 @@ function setup_node() {
                                                                 && rm node.tar.xz
                                                                 && mv node-v*-*-* \"${SCRIPT_DIR}/node\""
 
-    PATH="${SCRIPT_DIR}/node/bin:$PATH"
-    "${SCRIPT_DIR}/node/lib/node_modules/npm/bin/npm-cli.js" install
-
+    # only run install if needed
+    if [[ $? == 1 ]]; then
+        PATH="${SCRIPT_DIR}/node/bin:$PATH"
+        "${SCRIPT_DIR}/node/lib/node_modules/npm/bin/npm-cli.js" install
+    fi
     popd > /dev/null
 }
 
@@ -341,18 +348,36 @@ function start_process() {
 # Assumes the following are set
 # SCRIPT_DIR = The path to the root of the PixelStreamingInfrastructure repo.
 # NPM = The npm command path
-# SERVER_ARGS The arguments to be passed to the server
-function start_wilbur() {
+function build_wilbur() {
     pushd ${SCRIPT_DIR}/../../.. > /dev/null
 
+    pushd Common > /dev/null
+    if [[ ! -d "build" ]]; then
+        echo Building common
+        ${NPM} run build
+    fi
+    popd
+
     pushd Signalling > /dev/null
+    echo Building signalling
     ${NPM} link ../Common
     ${NPM} run build
     popd > /dev/null
 
     pushd SignallingWebServer > /dev/null
+    echo Building wilbur
     ${NPM} link ../Signalling
     ${NPM} run build
+
+    popd > /dev/null
+}
+
+# Assumes the following are set
+# SCRIPT_DIR = The path to the root of the PixelStreamingInfrastructure repo.
+# NPM = The npm command path
+# SERVER_ARGS The arguments to be passed to the server
+function start_wilbur() {
+    pushd ${SCRIPT_DIR}/../../../SignallingWebServer > /dev/null
 
     echo "Starting wilbur signalling server use ctrl-c to exit"
     echo "----------------------------------"
@@ -361,4 +386,3 @@ function start_wilbur() {
 
     popd > /dev/null
 }
-
