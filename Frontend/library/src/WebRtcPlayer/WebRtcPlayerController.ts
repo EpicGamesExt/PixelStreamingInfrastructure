4ca54484c8f8ebddac9110a48e92c102a335cf7d
Luke Bermingham <luke.bermingham@xa.epicgames.com>
2024-07-10 14:35:53 +1000

    Fix xr convergence and xr inputs

--- Frontend/library/src/WebRtcPlayer/WebRtcPlayerController.ts
+++ Frontend/library/src/WebRtcPlayer/WebRtcPlayerController.ts
@@ -709,6 +709,15 @@ export class WebRtcPlayerController {
         );
         this.streamMessageController.registerMessageHandler(
             MessageDirection.ToStreamer,
+            'XREyeViews',
+            (data: Array<number | string>) =>
+                this.sendMessageController.sendMessageToStreamer(
+                    'XREyeViews',
+                    data
+                )
+        );
+        this.streamMessageController.registerMessageHandler(
+            MessageDirection.ToStreamer,
             'XRHMDTransform',
             (data: Array<number | string>) =>
                 this.sendMessageController.sendMessageToStreamer(
@@ -740,6 +749,15 @@ export class WebRtcPlayerController {
             (data: Array<number | string>) =>
                 this.sendMessageController.sendMessageToStreamer(
                     'XRButtonTouched',
+                    data
+                )
+        );
+        this.streamMessageController.registerMessageHandler(
+            MessageDirection.ToStreamer,
+            'XRButtonTouchReleased',
+            (data: Array<number | string>) =>
+                this.sendMessageController.sendMessageToStreamer(
+                    'XRButtonTouchReleased',
                     data
                 )
         );
