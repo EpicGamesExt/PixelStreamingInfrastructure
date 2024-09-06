12bb47b4ec6604850e7bb2e48151b8774ff034ad
Matthew Cotton <matt@tensorworks.com.au>
2024-09-06 12:42:41 +1000

    Adding mouse movement tests for resizing the window

--- Extras/FrontendTests/tests/mouse.spec.ts
+++ Extras/FrontendTests/tests/mouse.spec.ts
@@ -146,7 +146,6 @@ test('Test hovering mouse movement', {
             return { width: videoElem.videoWidth, height: videoElem.videoHeight };
         });
 
-        // where the pointer anchors in locked mode changes by browser
         const anchor = { x: playerBox.x, y: playerBox.y };
         const coordConverter = new InputCoordTranslator();
         coordConverter.reconfigure(playerBox, videoSize);
@@ -157,9 +156,6 @@ test('Test hovering mouse movement', {
             { x: anchor.x + 200, y: anchor.y + 200 },
             { x: anchor.x + 200, y: anchor.y + 100 },
         ];
-
-        // click on stream to activate pointer lock
-        await page.mouse.click(anchor.x, anchor.y);
 
         // perform the actions
         const events = await getEventsFor(streamerPage, async () => {
@@ -175,5 +171,134 @@ test('Test hovering mouse movement', {
         const singlePlayerEvents = events[firstPlayerId];
         expect(singlePlayerEvents).toContainActions(expectedActions);
     }
+});
+
+test('Test mouse input after resizing. Hover mouse.', {
+    tag: ['@mouse'],
+}, async ({ page, streamerPage, streamerId }) => {
+
+    // helps debugging
+    // helpers.attachToConsoleEvents(streamerPage, (...args: any[]) => {
+    //     console.log("Streamer: ", ...args);
+    // });
+    //
+    // helpers.attachToConsoleEvents(page, (...args: any[]) => {
+    //     console.log("Player: ", ...args);
+    // });
+
+    await page.goto(`/?StreamerId=${streamerId}&HoveringMouse=true`);
+    await page.getByText('Click to start').click();
+
+    await helpers.waitForVideo(page);
+
+    // resize the window to be smaller
+    const oldSize = page.viewportSize();
+    expect(oldSize).not.toBeNull();
+    await page.setViewportSize({ width: oldSize!.width * 0.75, height: oldSize!.height * 0.5 });
+    await helpers.delay(1000);
+
+    const playerBox = await page.locator('#videoElementParent').boundingBox();
+    expect(playerBox).not.toBeNull();
+
+    const videoSize = await page.evaluate(()=>{
+        let videoElem = document.getElementById('streamingVideo') as HTMLVideoElement;
+        return { width: videoElem.videoWidth, height: videoElem.videoHeight };
+    });
+
+    const anchor = { x: playerBox!.x, y: playerBox!.y };
+    const coordConverter = new InputCoordTranslator();
+    coordConverter.reconfigure(playerBox!, videoSize);
+    const expectedActions: DataChannelEvent[] = [];
+    const movements = [
+        { x: anchor.x + 300, y: anchor.y + 0 },
+        { x: anchor.x + 300, y: anchor.y + 200 },
+        { x: anchor.x + 200, y: anchor.y + 200 },
+        { x: anchor.x + 200, y: anchor.y + 100 },
+    ];
+
+    // perform the actions
+    const events = await getEventsFor(streamerPage, async () => {
+        for (const movement of movements) {
+            await page.mouse.move(movement.x, movement.y);
+            const translated = coordConverter.translateUnsigned(movement.x, movement.y);
+            expectedActions.push({ type: PSEventTypes.MouseMove, x: Math.trunc(translated.x), y: Math.trunc(translated.y) });
+        }
+    });
+
+    // check we got the expected events
+    const firstPlayerId = Object.keys(events)[0];
+    const singlePlayerEvents = events[firstPlayerId];
+    expect(singlePlayerEvents).toContainActions(expectedActions);
+});
+
+test('Test mouse input after resizing. locked mouse.', {
+    tag: ['@mouse'],
+}, async ({ page, streamerPage, streamerId, browserName }) => {
+
+    // helps debugging
+    // helpers.attachToConsoleEvents(streamerPage, (...args: any[]) => {
+    //     console.log("Streamer: ", ...args);
+    // });
+    //
+    // helpers.attachToConsoleEvents(page, (...args: any[]) => {
+    //     console.log("Player: ", ...args);
+    // });
+
+    await page.goto(`/?StreamerId=${streamerId}&HoveringMouse=false`);
+    await page.getByText('Click to start').click();
+
+    await helpers.waitForVideo(page);
+
+    // resize the window to be smaller
+    const oldSize = page.viewportSize();
+    expect(oldSize).not.toBeNull();
+    await page.setViewportSize({ width: oldSize!.width * 0.75, height: oldSize!.height * 0.5 });
+    await helpers.delay(1000);
+
+    const playerBox = await page.locator('#videoElementParent').boundingBox();
+    expect(playerBox).not.toBeNull();
+
+    const videoSize = await page.evaluate(()=>{
+        let videoElem = document.getElementById('streamingVideo') as HTMLVideoElement;
+        return { width: videoElem.videoWidth, height: videoElem.videoHeight };
+    });
+
+    const coordConverter = new InputCoordTranslator();
+    coordConverter.reconfigure(playerBox!, videoSize);
+    const expectedActions: DataChannelEvent[] = [];
+    const movements = [
+        // Note: if these values go above the dimensions of the player they might get ignored
+        { x: 10, y: 0 },
+        { x: 0, y: 10 },
+        { x: -20, y: 0 },
+        { x: 0, y: -20 },
+        { x: 10, y: 10 },
+    ];
+    
+    // where the pointer anchors in locked mode changes by browser
+    const anchor: TranslatedCoordUnsigned = (() => {
+        if (browserName == 'firefox') {
+            return { x: playerBox!.x + playerBox!.width / 2, y: playerBox!.y + playerBox!.height / 2 };
+        } else {
+            return { x: playerBox!.x, y: playerBox!.y };
+        }
+    })();
+
+    // click on stream to activate pointer lock
+    await page.mouse.click(anchor.x, anchor.y);
+
+    // perform the actions
+    const events = await getEventsFor(streamerPage, async () => {
+        for (const movement of movements) {
+            await page.mouse.move(anchor.x + movement.x, anchor.y + movement.y);
+            const translated = coordConverter.translateSigned(movement.x, movement.y);
+            expectedActions.push({ type: PSEventTypes.MouseMove, deltaX: Math.trunc(translated.x), deltaY: Math.trunc(translated.y) });
+        }
+    });
+
+    // check we got the expected events
+    const firstPlayerId = Object.keys(events)[0];
+    const singlePlayerEvents = events[firstPlayerId];
+    expect(singlePlayerEvents).toContainActions(expectedActions);
 });
 
