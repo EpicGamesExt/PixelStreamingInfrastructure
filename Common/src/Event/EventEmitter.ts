1d29db39bedd5f3e1962dab9648c71f23e993f3b
Luke Bermingham <luke.bermingham@xa.epicgames.com>
2024-11-27 10:12:38 +1000

    Fix linting issues in Common

--- Common/src/Event/EventEmitter.ts
+++ Common/src/Event/EventEmitter.ts
@@ -1,27 +1,27 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 class PixelStreamingEventListener implements EventListenerObject {
+    private _callback: (...args: unknown[]) => void;
+    private _args: unknown[] = [];
 
-	private _callback: Function;
-	private _args: any[] = [];
+    constructor(callback: (...args: unknown[]) => void) {
+        this._callback = callback;
+    }
 
-	constructor(callback: Function) {
-		this._callback = callback;
-	}
+    handleEvent(_evt: Event): void {
+        this._callback(...this._args);
+        // Reset storage of args.
+        this._args = [];
+    }
 
-	handleEvent(evt: Event): void {
-		this._callback(...this._args);
-	}
-
-	set args(value: any) {
-		this._args = value;
-	}
-
+    setArgs(...args: unknown[]) {
+        this._args = args;
+    }
 }
 
 interface EventListenerPair {
-	callback: Function;
-	eventListenerWrapper: PixelStreamingEventListener;
+    callback: (...args: unknown[]) => void;
+    eventListenerWrapper: PixelStreamingEventListener;
 }
 
 /**
@@ -56,223 +56,222 @@ interface EventListenerPair {
  * ```
  */
 export class EventEmitter extends EventTarget {
+    private _eventListeners: Map<string, Array<EventListenerPair>>;
 
-	private _eventListeners: Map<string, Array<EventListenerPair>>;
+    constructor() {
+        super();
+        this._eventListeners = new Map<string, Array<EventListenerPair>>();
+    }
 
-	constructor(){
-		super();
-		this._eventListeners = new Map<string, Array<EventListenerPair>>
-	}
+    private removeListenerInternal(eventName: string, listener: (...args: unknown[]) => void): this {
+        if (this._eventListeners.has(eventName)) {
+            const listeners: Array<EventListenerPair> | undefined = this._eventListeners.get(eventName);
 
-	private removeListenerInternal(eventName: string, listener: Function) : this {
-		if(this._eventListeners.has(eventName)) {
-			const listeners: Array<EventListenerPair> | undefined = this._eventListeners.get(eventName);
+            if (listeners === undefined) {
+                return this;
+            }
 
-			if(listeners === undefined) {
-				return this;
-			}
+            for (let i = 0; i < listeners.length; ++i) {
+                const eventPair: EventListenerPair = listeners[i];
+                if (eventPair.callback === listener) {
+                    // Remove from event target
+                    super.removeEventListener(eventName, eventPair.eventListenerWrapper);
+                    // Remove from our internal map
+                    listeners.splice(i, 1);
+                    break;
+                }
+            }
+        }
+        return this;
+    }
 
-			for(let i = 0; i < listeners.length; ++i) {
-				let eventPair: EventListenerPair = listeners[i];
-				if(eventPair.callback === listener) {
-					// Remove from event target
-					super.removeEventListener(eventName, eventPair.eventListenerWrapper);
-					// Remove from our internal map
-					listeners.splice(i, 1);
-					break;
-				}
-			}
-		}
-		return this;
-	}
+    /**
+     * Alias for `emitter.on(eventName, listener)`.
+     */
+    addListener(eventName: string, listener: (...args: unknown[]) => void): this {
+        return this.on(eventName, listener);
+    }
 
-	/**
-	 * Alias for `emitter.on(eventName, listener)`.
-	 */
-	addListener(eventName: string, listener: Function) : this {
-		return this.on(eventName, listener);
-	}
+    /**
+     * Adds the `listener` function to the end of the listeners array for the event
+     * named `eventName`.
+     *
+     * ```js
+     * server.on('connection', (stream) => {
+     *   console.log('someone connected!');
+     * });
+     * ```
+     *
+     * Returns a reference to the `EventEmitter`, so that calls can be chained.
+     *
+     * @param eventName - The name of the event.
+     * @param listener - The callback function
+     */
+    on(eventName: string, listener: (...args: unknown[]) => void): this {
+        // Wrap our normal JS function in a event listener interface
+        // so we can use it with event target.
+        const eventListenerWrapper: PixelStreamingEventListener = new PixelStreamingEventListener(listener);
 
-	/**
-	 * Adds the `listener` function to the end of the listeners array for the event
-	 * named `eventName`.
-	 *
-	 * ```js
-	 * server.on('connection', (stream) => {
-	 *   console.log('someone connected!');
-	 * });
-	 * ```
-	 *
-	 * Returns a reference to the `EventEmitter`, so that calls can be chained.
-	 *
-	 * @param eventName The name of the event.
-	 * @param listener The callback function
-	 */
-	on(eventName: string, listener: Function): this {
+        super.addEventListener(eventName, eventListenerWrapper);
 
-		// Wrap our normal JS function in a event listener interface
-		// so we can use it with event target.
-		const eventListenerWrapper: PixelStreamingEventListener = new PixelStreamingEventListener(listener);
+        // Store the event listener/function pair for later removal.
+        if (!this._eventListeners.has(eventName)) {
+            this._eventListeners.set(eventName, new Array<EventListenerPair>());
+        }
 
-		super.addEventListener(eventName, eventListenerWrapper);
+        this._eventListeners
+            .get(eventName)
+            ?.push({ callback: listener, eventListenerWrapper: eventListenerWrapper });
 
-		// Store the event listener/function pair for later removal.
-		if(!this._eventListeners.has(eventName)){
-			this._eventListeners.set(eventName, new Array<EventListenerPair>());
-		}
+        return this;
+    }
 
-		this._eventListeners.get(eventName)?.push({ callback: listener, eventListenerWrapper: eventListenerWrapper })
+    /**
+     * Adds a **one-time** `listener` function for the event named `eventName`. The
+     * next time `eventName` is triggered, this listener is removed and then invoked.
+     *
+     * ```js
+     * server.once('connection', (stream) => {
+     *   console.log('Ah, we have our first user!');
+     * });
+     * ```
+     *
+     * Returns a reference to the `EventEmitter`, so that calls can be chained.
+     * @param eventName - The name of the event.
+     * @param listener - The callback function
+     */
+    once(eventName: string, listener: (...args: unknown[]) => void): this {
+        // Pass options so this event callback is only called once
+        const eventListenerOpts: AddEventListenerOptions = { once: true };
 
-		return this;
-	}
+        // Wrap our normal JS function in a event listener interface
+        // so we can use it with event target and remove it from event target when this function completes.
+        const eventListenerWrapper: PixelStreamingEventListener = new PixelStreamingEventListener(
+            (...args: unknown[]) => {
+                listener(args);
+                this.removeListenerInternal(eventName, listener);
+            }
+        );
 
-	/**
-	 * Adds a **one-time** `listener` function for the event named `eventName`. The
-	 * next time `eventName` is triggered, this listener is removed and then invoked.
-	 *
-	 * ```js
-	 * server.once('connection', (stream) => {
-	 *   console.log('Ah, we have our first user!');
-	 * });
-	 * ```
-	 *
-	 * Returns a reference to the `EventEmitter`, so that calls can be chained.
-	 * @param eventName The name of the event.
-	 * @param listener The callback function
-	 */
-	once(eventName: string, listener: Function): this {
+        super.addEventListener(eventName, eventListenerWrapper, eventListenerOpts);
 
-		// Pass options so this event callback is only called once
-		const eventListenerOpts : AddEventListenerOptions = { once: true };
+        // Store the event listener/function pair for later removal.
+        if (!this._eventListeners.has(eventName)) {
+            this._eventListeners.set(eventName, new Array<EventListenerPair>());
+        }
 
-		// Wrap our normal JS function in a event listener interface
-		// so we can use it with event target and remove it from event target when this function completes.
-		const eventListenerWrapper: PixelStreamingEventListener = new PixelStreamingEventListener((...args : any[]) => {
-			listener(args);
-			this.removeListenerInternal(eventName, listener);
-		});
+        this._eventListeners
+            .get(eventName)
+            ?.push({ callback: listener, eventListenerWrapper: eventListenerWrapper });
 
-		super.addEventListener(eventName, eventListenerWrapper, eventListenerOpts);
+        return this;
+    }
 
-		// Store the event listener/function pair for later removal.
-		if(!this._eventListeners.has(eventName)){
-			this._eventListeners.set(eventName, new Array<EventListenerPair>());
-		}
+    /**
+     * Removes the specified `listener` from this EventEmitter.
+     *
+     * ```js
+     * const callback = (stream) => {
+     *   console.log('someone connected!');
+     * };
+     * server.on('connection', callback);
+     * // ...
+     * server.removeListener('connection', callback);
+     * ```
+     * Returns a reference to the `EventEmitter`, so that calls can be chained.
+     */
+    removeListener(eventName: string, listener: (...args: unknown[]) => void): this {
+        this.removeListenerInternal(eventName, listener);
+        return this;
+    }
 
-		this._eventListeners.get(eventName)?.push({ callback: listener, eventListenerWrapper: eventListenerWrapper })
+    /**
+     * Alias for `emitter.removeListener()`.
+     */
+    off(eventName: string, listener: (...args: unknown[]) => void): this {
+        return this.removeListener(eventName, listener);
+    }
 
-		return this;
-	}
+    /**
+     * Removes all listeners, or those of the specified `eventName`.
+     * Returns a reference to the `EventEmitter`, so that calls can be chained.
+     */
+    removeAllListeners(eventName: string): this {
+        if (this._eventListeners.has(eventName)) {
+            const listeners: Array<EventListenerPair> | undefined = this._eventListeners.get(eventName);
 
-	/**
-	 * Removes the specified `listener` from this EventEmitter.
-	 *
-	 * ```js
-	 * const callback = (stream) => {
-	 *   console.log('someone connected!');
-	 * };
-	 * server.on('connection', callback);
-	 * // ...
-	 * server.removeListener('connection', callback);
-	 * ```
-	 * Returns a reference to the `EventEmitter`, so that calls can be chained.
-	 */
-	removeListener(eventName: string, listener: Function): this
-	{
-		this.removeListenerInternal(eventName, listener);
-		return this;
-	}
+            if (listeners === undefined) {
+                return this;
+            }
 
-	/**
-	 * Alias for `emitter.removeListener()`.
-	 */
-	off(eventName: string, listener: Function): this {
-		return this.removeListener(eventName, listener);
-	}
+            // Remove each event listener from the event target
+            for (const listenerPair of listeners) {
+                this.removeEventListener(eventName, listenerPair.eventListenerWrapper);
+            }
 
-	/**
-	 * Removes all listeners, or those of the specified `eventName`.
-	 * Returns a reference to the `EventEmitter`, so that calls can be chained.
-	 */
-	removeAllListeners(eventName: string): this {
-		if(this._eventListeners.has(eventName)){
+            // Remove all event listeners mapped to this event from our internal map
+            this._eventListeners.delete(eventName);
+        }
+        return this;
+    }
 
-			const listeners: Array<EventListenerPair> | undefined = this._eventListeners.get(eventName);
+    /**
+     * Synchronously calls each of the listeners registered for the event named `eventName`, in the order they were registered, passing the supplied arguments
+     * to each.
+     *
+     * Returns `true` if the event had listeners, `false` otherwise.
+     *
+     * ```js
+     * import { EventEmitter } from 'node:events';
+     * const myEmitter = new EventEmitter();
+     *
+     * // First listener
+     * myEmitter.on('event', function firstListener() {
+     *   console.log('Helloooo! first listener');
+     * });
+     * // Second listener
+     * myEmitter.on('event', function secondListener(arg1, arg2) {
+     *   console.log(`event with parameters ${arg1}, ${arg2} in second listener`);
+     * });
+     * // Third listener
+     * myEmitter.on('event', function thirdListener(...args) {
+     *   const parameters = args.join(', ');
+     *   console.log(`event with parameters ${parameters} in third listener`);
+     * });
+     *
+     * console.log(myEmitter.listeners('event'));
+     *
+     * myEmitter.emit('event', 1, 2, 3, 4, 5);
+     *
+     * // Prints:
+     * // [
+     * //   [Function: firstListener],
+     * //   [Function: secondListener],
+     * //   [Function: thirdListener]
+     * // ]
+     * // Helloooo! first listener
+     * // event with parameters 1, 2 in second listener
+     * // event with parameters 1, 2, 3, 4, 5 in third listener
+     * ```
+     */
+    emit(eventName: string, ...args: unknown[]): boolean {
+        if (this._eventListeners.has(eventName)) {
+            const listeners: Array<EventListenerPair> | undefined = this._eventListeners.get(eventName);
 
-			if(listeners === undefined) {
-				return this;
-			}
+            if (listeners === undefined) {
+                return false;
+            }
 
-			// Remove each event listener from the event target
-			for(let listenerPair of listeners) {
-				this.removeEventListener(eventName, listenerPair.eventListenerWrapper);
-			}
+            // Ensure each of our listeners have the args the callback injected
+            for (const listenerPair of listeners) {
+                listenerPair.eventListenerWrapper.setArgs(args);
+            }
 
-			// Remove all event listeners mapped to this event from our internal map
-			this._eventListeners.delete(eventName);
-		}
-		return this;
-	}
+            // Fire off the actual event
+            super.dispatchEvent(new Event(eventName));
 
-	/**
-	 * Synchronously calls each of the listeners registered for the event named `eventName`, in the order they were registered, passing the supplied arguments
-	 * to each.
-	 *
-	 * Returns `true` if the event had listeners, `false` otherwise.
-	 *
-	 * ```js
-	 * import { EventEmitter } from 'node:events';
-	 * const myEmitter = new EventEmitter();
-	 *
-	 * // First listener
-	 * myEmitter.on('event', function firstListener() {
-	 *   console.log('Helloooo! first listener');
-	 * });
-	 * // Second listener
-	 * myEmitter.on('event', function secondListener(arg1, arg2) {
-	 *   console.log(`event with parameters ${arg1}, ${arg2} in second listener`);
-	 * });
-	 * // Third listener
-	 * myEmitter.on('event', function thirdListener(...args) {
-	 *   const parameters = args.join(', ');
-	 *   console.log(`event with parameters ${parameters} in third listener`);
-	 * });
-	 *
-	 * console.log(myEmitter.listeners('event'));
-	 *
-	 * myEmitter.emit('event', 1, 2, 3, 4, 5);
-	 *
-	 * // Prints:
-	 * // [
-	 * //   [Function: firstListener],
-	 * //   [Function: secondListener],
-	 * //   [Function: thirdListener]
-	 * // ]
-	 * // Helloooo! first listener
-	 * // event with parameters 1, 2 in second listener
-	 * // event with parameters 1, 2, 3, 4, 5 in third listener
-	 * ```
-	 */
-	emit(eventName: string, ...args: any[]): boolean {
-		if(this._eventListeners.has(eventName)){
-
-			const listeners: Array<EventListenerPair> | undefined = this._eventListeners.get(eventName);
-
-			if(listeners === undefined) {
-				return false;
-			}
-
-			// Ensure each of our listeners have the args the callback injected
-			for(let listenerPair of listeners) {
-				listenerPair.eventListenerWrapper.args = args;
-			}
-
-			// Fire off the actual event
-			super.dispatchEvent(new Event(eventName));
-
-			return true;
-		}
-		return false;
-	}
-
+            return true;
+        }
+        return false;
+    }
 }
