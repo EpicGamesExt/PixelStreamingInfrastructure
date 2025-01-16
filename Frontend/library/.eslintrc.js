72a22de235973b89e52ca1d1516d9ba732c0f9ff
Matthew.Cotton <matt@tensorworks.com.au>
2025-01-16 14:40:45 +1100

    Prettier rules into repo root. Configured eslint projects with config roots so they work with lint-staged.

--- Frontend/library/.eslintrc.js
+++ Frontend/library/.eslintrc.js
@@ -3,7 +3,10 @@
 module.exports = {
     root: true,
     parser: '@typescript-eslint/parser',
-    parserOptions: { project: './tsconfig.cjs.json' },
+    parserOptions: {
+        project: './tsconfig.cjs.json',
+        tsconfigRootDir: __dirname,
+    },
     plugins: ['@typescript-eslint'],
     extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', "plugin:prettier/recommended"],
     rules: {
