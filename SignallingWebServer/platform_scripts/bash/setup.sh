48b91795069558a57a9f7b89a6d9338a68d4dc07
Eden Harris <eden@tensorworks.com.au>
2025-02-25 18:04:43 +1100

    Fix infra scripts when a space is in the file path. Add quotes to multiple places in the scripts to handle spaces.
    (cherry picked from commit b4d2f1f082ab63695373f768a3fa0ac8339668a2)

--- SignallingWebServer/platform_scripts/bash/setup.sh
+++ SignallingWebServer/platform_scripts/bash/setup.sh
@@ -3,7 +3,7 @@
 # Copyright Epic Games, Inc. All Rights Reserved.
 SCRIPT_DIR="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
 
-. ${SCRIPT_DIR}/common.sh
+. "${SCRIPT_DIR}/common.sh"
 
 parse_args $@
 setup $@
