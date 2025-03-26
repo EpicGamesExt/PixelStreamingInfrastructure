adb0cadc83928f8965343fbaee67b44ed0fe25df
Matthew.Cotton <matt@tensorworks.com.au>
2025-03-27 10:31:28 +1100

    Adding links to the changelog generator

--- Extras/ChangelogGenerator/src/functions.ts
+++ Extras/ChangelogGenerator/src/functions.ts
@@ -5,12 +5,18 @@ import parse from 'remark-parse';
 import stringify from 'remark-stringify';
 import { RootContent } from 'mdast';
 
+function getUrl() {
+    const gitRemote = execSync(`git remote get-url origin`, { encoding: 'utf-8' }).trim();
+    return gitRemote.replace(/^git@([^:]+):(.+)\.git$/, 'https://$1/$2');
+}
+
 export function clearChanges(_logfilePath: string) {
     const template = readFileSync(_logfilePath);
     const tree = remark().use(parse).parse(template);
 
-    tree.children = tree.children.slice(0,
-        tree.children.findIndex((node) => node.type === 'html' && node.value.includes('<!-- BEGIN -->')) + 1,
+    tree.children = tree.children.slice(
+        0,
+        tree.children.findIndex((node) => node.type === 'html' && node.value.includes('<!-- BEGIN -->')) + 1
     );
 
     const modifiedMarkdown = remark().use(stringify, { bullet: '-' }).stringify(tree);
@@ -18,45 +24,78 @@ export function clearChanges(_logfilePath: string) {
 }
 
 export function addVersionChanges(
-    _logfilePath: string,
-    _packagePath: string,
-    _previousTag: string,
-    _currentTag: string,
-    _versionLabel: string
+    logfilePath: string,
+    packagePath: string,
+    previousTag: string,
+    currentTag: string,
+    versionLabel: string
 ) {
+    const repoUrl = getUrl();
+
     // get log entries
-    const logEntries = execSync(
-        `git log ${_previousTag}..${_currentTag} --oneline --pretty=format:%s ${_packagePath}`,
-        { encoding: 'utf-8' }
-    );
+    const logEntries = execSync(`git log ${previousTag}..${currentTag} --oneline ${packagePath}`, {
+        encoding: 'utf-8'
+    });
 
     // build the new changelog entry
     const newNodes: RootContent[] = [
         {
             type: 'heading',
             depth: 2,
-            children: [{ type: 'text', value: `${_versionLabel}` }]
+            children: [
+                {
+                    type: 'link',
+                    url: `${repoUrl}/releases/tag/${currentTag}`,
+                    children: [{ type: 'text', value: `${versionLabel}` }]
+                }
+            ]
         },
         {
             type: 'list',
             ordered: false,
             spread: false,
-            children: logEntries.split('\n').map((comment) => {
-                return {
-                    type: 'listItem',
-                    spread: false,
-                    children: [
-                        {
-                            type: 'paragraph',
-                            children: [{ type: 'text', value: comment }]
-                        }
-                    ]
-                };
-            })
+            children: logEntries
+                .trim()
+                .split('\n')
+                .map((comment) => {
+                    const matches = comment.match(/([0-9a-z]+) (.*)$/);
+                    if (matches) {
+                        const commitHash = matches[1];
+                        const commitMessage = matches[2];
+                        return {
+                            type: 'listItem',
+                            spread: false,
+                            children: [
+                                {
+                                    type: 'paragraph',
+                                    children: [
+                                        {
+                                            type: 'link',
+                                            url: `${repoUrl}/commit/${commitHash}`,
+                                            children: [{ type: 'text', value: commitHash }]
+                                        },
+                                        { type: 'text', value: ` ${commitMessage}` }
+                                    ]
+                                }
+                            ]
+                        };
+                    } else {
+                        return {
+                            type: 'listItem',
+                            spread: false,
+                            children: [
+                                {
+                                    type: 'paragraph',
+                                    children: [{ type: 'text', value: comment }]
+                                }
+                            ]
+                        };
+                    }
+                })
         }
     ];
 
-    const template = readFileSync(_logfilePath);
+    const template = readFileSync(logfilePath);
     const tree = remark().use(parse).parse(template);
 
     tree.children.splice(
@@ -66,5 +105,5 @@ export function addVersionChanges(
     );
 
     const modifiedMarkdown = remark().use(stringify, { bullet: '-' }).stringify(tree);
-    writeFileSync(_logfilePath, modifiedMarkdown);
+    writeFileSync(logfilePath, modifiedMarkdown);
 }
