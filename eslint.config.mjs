0d751c50d664b8aa5271316d39fec7f2b7e6fb3a
mcottontensor <80377552+mcottontensor@users.noreply.github.com>
2025-03-28 10:33:28 +1100

    Merge pull request #549 from mcottontensor/eslint-plugin-fix
    Removed the eslint plugin from the dependencies.
    
    (cherry picked from commit da7ed8f4e1aed86e1bcdc928e0cca37c9c0806ed)
    
    # Conflicts:
    #	package-lock.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@
 import eslint from '@eslint/js';
 import prettierPluginRecommended from 'eslint-plugin-prettier/recommended';
 import tseslint from 'typescript-eslint';
-import checkCopyrightPlugin from '@epicgames-ps/eslint-plugin-check-copyright'
+import checkCopyrightPlugin from './Extras/eslint/plugin-check-copyright/index.js'
 
 export default tseslint.config(
     eslint.configs.recommended,
