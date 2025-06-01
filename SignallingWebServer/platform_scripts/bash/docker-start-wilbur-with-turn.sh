44fdc20b3aab7fd9748fb5c3779e6f1acdad5771
mcottontensor <80377552+mcottontensor@users.noreply.github.com>
2025-06-02 09:12:35 +1000

    chore(platform_scripts): Updating scripts to remove default-stun/turn. (#659)
    These arguments no longer have any functionality and instead will cause
    an error.
    
    (cherry picked from commit e1baf1a6bad5fedf85217e34c2f8c623a311a352)

--- SignallingWebServer/platform_scripts/bash/docker-start-wilbur-with-turn.sh
+++ SignallingWebServer/platform_scripts/bash/docker-start-wilbur-with-turn.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Copyright Epic Games, Inc. All Rights Reserved.
 
-docker run --name wilbur_latest --network host --rm wilbur --default-turn
-
+docker run --name wilbur_latest --network host --rm wilbur
