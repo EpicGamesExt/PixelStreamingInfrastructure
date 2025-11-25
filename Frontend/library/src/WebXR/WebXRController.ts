597a058ef38b376b0258016aea802da4b076687b
Leon Radley <leon@radley.se>
2025-11-25 03:29:29 +0100

    SecurityError: Failed to execute 'isSessionSupported' on 'XRSystem' when in iframe (#734)
    * fix: SecurityError: Failed to execute 'isSessionSupported' on 'XRSystem' when in iframe
    
    fixes: #722
    
    * Update Frontend/library/src/WebXR/WebXRController.ts
    
    * Update Frontend/library/src/WebXR/WebXRController.ts
    
    ---------
    
    Co-authored-by: Luke Bermingham <1215582+lukehb@users.noreply.github.com>
    (cherry picked from commit cc8c923702af288e8a74265978831acfee52627f)

--- Frontend/library/src/WebXR/WebXRController.ts
+++ Frontend/library/src/WebXR/WebXRController.ts
@@ -485,12 +485,16 @@ export class WebXRController {
             Logger.Info('WebXR requires https, if you want WebXR use https.');
         }
 
-        if (navigator.xr) {
-            return navigator.xr.isSessionSupported(mode);
-        } else {
-            return new Promise<boolean>(() => {
-                return false;
-            });
+        // Wrap in try-catch because access to XR object can be denied due
+        // to browser security permissions (e.g. streaming from an iframe)
+        try {
+            if (navigator.xr) {
+                return navigator.xr.isSessionSupported(mode);
+            }
+        } catch {
+            return Promise.resolve(false);
         }
+
+        return Promise.resolve(false);
     }
 }
